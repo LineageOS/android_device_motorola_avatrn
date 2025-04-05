e883d5c306b489015eb85fb6df73f6f41eb8f99e
Michael Bestas <mkbestas@lineageos.org>
2025-04-05 19:35:02 +0300

    avatrn: Add notification LED support
    Change-Id: I486938164462ae7ebe5867e74c4f77142f5ed14f

--- device.mk
+++ device.mk
@@ -57,6 +57,10 @@ PRODUCT_PACKAGES += \
     init.mmi.overlay.rc \
     init.vendor.st21nfc.rc
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light-service.lineage
+
 # LiveDisplay
 $(call soong_config_set_bool,livedisplay_sysfs,enable_af,true)
 
