ec3281605f89cc2d486536d20c1795cfe6a14121
Bruno Martins <bgcngm@gmail.com>
2025-05-31 13:01:12 +0100

    avatrn: Move and recategorize compat matrix and manifest flags
    Qualcomm also sets these in (common) product makefiles.
    
    Change-Id: I7139cba92454106fbca5f6105b966a43e70fe9d6

--- device.mk
+++ device.mk
@@ -75,5 +75,13 @@ $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/power_supp
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
+# VINTF
+ODM_MANIFEST_SKUS += dn n
+ODM_MANIFEST_DN_FILES := \
+    $(LOCAL_PATH)/manifests/manifest_dn.xml
+ODM_MANIFEST_N_FILES := \
+    $(LOCAL_PATH)/manifests/manifest_n.xml \
+    device/motorola/sm7435-common/vintf/manifest_ss.xml
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/motorola/avatrn/avatrn-vendor.mk)
